49f00a654c8df606a9ff62aa6dd883a2482764f7
arpitrana717 <arpitrana.underpinservices@gmail.com>
2024-07-22 12:11:22 +0530

    removed:build&node_moudules

--- build/jsb-default/src/settings.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"android",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/login.fire",orientation:"",server:"",jsList:[]};
--- build/web-desktop/src/settings.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/login.fire",orientation:"portrait",jsList:[]};
